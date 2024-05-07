f93ca1852c1fcbef5c71df2206b6e9d069c837e8
itsDamon <nick.parise20@gmail.com>
2024-05-07 14:54:29 +0200

    css register.php

--- login.php
+++ login.php
@@ -11,15 +11,14 @@
     <div class="form-floating mb-3">
         <input class="form-control" id="password_input" type="password" name="password" placeholder="password" required>
         <label for="password_input">Password</label>
-
     </div>
     <input class="btn btn-outline-secondary" type="submit" value="Login">
     <hr class="w-100">
 </form>
-    <a href="register.php">
-        <button class="btn btn-outline-secondary">
-            Register
-        </button>
-    </a>
+<a href="register.php">
+    <button class="btn btn-outline-secondary">
+        Register
+    </button>
+</a>
 
 <?php require_once 'footer.html' ?>
--- register.php
+++ register.php
@@ -1,27 +1,25 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<?php require_once 'header.html'; ?>
 
-<form action="register_validation.php" method="post">
-    <input type="text" name="name" placeholder="name" required>
-    <br>
-    <input type="text" name="surname" placeholder="surname" required>
-    <br>
-    <input type="text" name="username" placeholder="username or email" required>
-    <br>
-    <input type="password" name="password" placeholder="password" required>
-    <br>
-    <input type="submit" value="login">
-    <br>
+<form class="d-flex align-items-center justify-content center flex-column mb-2" action="register_validation.php"
+      method="post">
+    <div class="form-floating mb-3">
+        <input class="form-control" type="text" name="name" id="name_input" placeholder="name" required>
+        <label for="name_input">Name</label>
+    </div>
+    <div class="form-floating mb-3">
+        <input class="form-control" type="text" name="surname" id="surname_input" placeholder="surname" required>
+        <label for="surname_input">Surname</label>
+    </div>
+    <div class="form-floating mb-3">
+        <input class="form-control" type="text" name="username" id="username_input" placeholder="username or email"
+               required>
+        <label for="username_input">Username or Email</label>
+    </div>
+    <div class="form-floating mb-3">
+        <input class="form-control" type="password" name="password" id="password_input" placeholder="password" required>
+        <label for="password_input">Password</label>
+    </div>
+    <input class="btn btn-outline-secondary" type="submit" value="Sign In">
 </form>
-<a href="login.php">Login</a>
 
-</body>
-</html>
+<?php require_once 'footer.html'; ?>
