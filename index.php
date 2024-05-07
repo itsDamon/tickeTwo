9481f257b39294f0b43f3a5eb6b00119bcb02e42
itsDamon <nick.parise20@gmail.com>
2024-05-07 13:53:59 +0200

    aggiunta header e footer

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
+require_once "header.html";
 session_start();
 if (isset($_SESSION['user'])) {
     echo "utente loggato";
 } else {
     echo "utente non loggato";
+require_once "footer.html";
 }
--- login.php
+++ login.php
@@ -1,13 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<?php require_once 'header.html' ?>
 
 <form action="login_validation.php" method="post">
     <input type="text" name="username" required>
@@ -19,5 +10,5 @@
 </form>
 
 <a href="register.php">Register</a>
-</body>
-</html>
+
+<?php require_once 'footer.html' ?>
