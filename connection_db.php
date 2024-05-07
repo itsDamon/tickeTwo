20da0b8ecd0a5b0bac03d811667c0a4fd299df08
mirkodavenia <mirkettorussel@gmail.com>
2024-05-07 11:23:44 +0200

    aggiunta pdo

---
+++ connection_db.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+PDO = PHP Data Object
+DSN = Data Source Name (tipo di db, nome host, nome del database, porta, charset)
+PDO => DSN, username, password, opzioni
+*/
+$type = 'mysql';
+$server = '127.0.0.1';
+$db = 'festival';
+$port = '3306';
+$charset = 'utf8mb4';
+
+$username = 'root';
+$password = 'root';
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+$dsn = "$type:host=$server;dbname=$db;port=$port;charset=$charset";
+$pdo = null;
+try {
+    $pdo = new PDO($dsn, $username, $password, $options);
+} catch (PDOException $e) {
+    throw new PDOException($e->getMessage(), $e->getCode());
+}
+?>
