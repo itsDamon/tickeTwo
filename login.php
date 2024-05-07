16dfdc16e6611c681493b7aa74fda4dff67fe2c4
itsDamon <nick.parise20@gmail.com>
2024-05-07 14:36:05 +0200

    css login.php

--- login.php
+++ login.php
@@ -1,14 +1,25 @@
 <?php require_once 'header.html' ?>
 
-<form action="login_validation.php" method="post">
-    <input type="text" name="username" required>
-    <br>
-    <input type="password" name="password" required>
-    <br>
-    <input type="submit" value="login">
-    <br>
-</form>
+<form class="d-flex align-items-center justify-content center flex-column mb-2" action="login_validation.php"
+      method="post">
+    <div class="form-floating mb-3">
+        <input class="form-control" id="name_input" type="text" name="username"
+               placeholder="Username or Email"
+               required>
+        <label for="name_input">Username or Email</label>
+    </div>
+    <div class="form-floating mb-3">
+        <input class="form-control" id="password_input" type="password" name="password" placeholder="password" required>
+        <label for="password_input">Password</label>
 
-<a href="register.php">Register</a>
+    </div>
+    <input class="btn btn-outline-secondary" type="submit" value="Login">
+    <hr class="w-100">
+    <a href="register.php">
+        <button class="btn btn-outline-secondary">
+            Register
+        </button>
+    </a>
+</form>
 
 <?php require_once 'footer.html' ?>
