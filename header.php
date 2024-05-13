13e6fa7640ab22340289133a44f1342c8e7bd5ea
itsDamon <nick.parise20@gmail.com>
2024-05-13 14:21:36 +0200

    aggiunta carousel template

--- header.php
+++ header.php
@@ -13,6 +13,12 @@ date_default_timezone_set('Europe/Rome');
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous" defer></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+          integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
 </head>
 <body class="d-flex flex-column min-vh-100 flex-grow-1 p-3">
 <header class="text-center p-2 d-flex align-items-center justify-content-around">
@@ -25,5 +31,5 @@ date_default_timezone_set('Europe/Rome');
         <a href="profile.php">Profile</a>
     <?php } ?>
 </header>
-<div class="mt-auto d-flex align-items-center justify-content-center flex-column">
+<div class="mt-auto d-flex align-items-center justify-content-center flex-column h-100"> <!--mt-auto d-flex align-items-center justify-content-center flex-column-->
 
--- home.php
+++ home.php
@@ -1,9 +1,217 @@
 <?php
 require_once "header.php";
 session_start();
+?>
+    <style>
+        @media (max-width: 768px) {
+            .carousel-inner .carousel-item > div {
+                display: none;
+            }
+
+            .carousel-inner .carousel-item > div:first-child {
+                display: block;
+            }
+        }
+
+        .carousel-inner .carousel-item.active,
+        .carousel-inner .carousel-item-start,
+        .carousel-inner .carousel-item-next,
+        .carousel-inner .carousel-item-prev {
+            display: flex;
+        / / transition-duration: 8 s;
+        }
+
+        /* display 4 */
+        @media (min-width: 768px) {
+            .carousel-inner .carousel-item-right.active,
+            .carousel-inner .carousel-item-next,
+            .carousel-item-next:not(.carousel-item-start) {
+                transform: translateX(25%) !important;
+            }
+
+            .carousel-inner .carousel-item-left.active,
+            .carousel-item-prev:not(.carousel-item-end),
+            .active.carousel-item-start,
+            .carousel-item-prev:not(.carousel-item-end) {
+                transform: translateX(-25%) !important;
+            }
+
+            .carousel-item-next.carousel-item-start, .active.carousel-item-end {
+                transform: translateX(0) !important;
+            }
+
+            .carousel-inner .carousel-item-prev,
+            .carousel-item-prev:not(.carousel-item-end) {
+                transform: translateX(-25%) !important;
+            }
+        }
+    </style>
+
+    <div id="event-carousel" class="carousel slide container" data-bs-ride="carousel">
+        <div class="carousel-inner w-100">
+            <div class="carousel-item active">
+                <div class="col-md-3">
+                    <div class="card card-body">
+                        <img class="img-fluid" src="http://placehold.it/380?text=1">
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="col-md-3">
+                    <div class="card card-body">
+                        <img class="img-fluid" src="http://placehold.it/380?text=2">
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="col-md-3">
+                    <div class="card card-body">
+                        <img class="img-fluid" src="http://placehold.it/380?text=3">
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="col-md-3">
+                    <div class="card card-body">
+                        <img class="img-fluid" src="http://placehold.it/380?text=4">
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="col-md-3">
+                    <div class="card card-body">
+                        <img class="img-fluid" src="http://placehold.it/380?text=5">
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="col-md-3">
+                    <div class="card card-body">
+                        <img class="img-fluid" src="http://placehold.it/380?text=6">
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="col-md-3">
+                    <div class="card card-body">
+                        <img class="img-fluid" src="http://placehold.it/380?text=7">
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="col-md-3">
+                    <div class="card card-body">
+                        <img class="img-fluid" src="http://placehold.it/380?text=8">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#event-carousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#event-carousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
+    <hr class="w-50">
+
+    <div id="artist-carousel" class="carousel slide container" data-bs-ride="carousel">
+        <div class="carousel-inner w-100">
+            <div class="carousel-item active">
+                <div class="col-md-3">
+                    <div class="card card-body">
+                        <img class="img-fluid" src="http://placehold.it/380?text=1">
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="col-md-3">
+                    <div class="card card-body">
+                        <img class="img-fluid" src="http://placehold.it/380?text=2">
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="col-md-3">
+                    <div class="card card-body">
+                        <img class="img-fluid" src="http://placehold.it/380?text=3">
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="col-md-3">
+                    <div class="card card-body">
+                        <img class="img-fluid" src="http://placehold.it/380?text=4">
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="col-md-3">
+                    <div class="card card-body">
+                        <img class="img-fluid" src="http://placehold.it/380?text=5">
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="col-md-3">
+                    <div class="card card-body">
+                        <img class="img-fluid" src="http://placehold.it/380?text=6">
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="col-md-3">
+                    <div class="card card-body">
+                        <img class="img-fluid" src="http://placehold.it/380?text=7">
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="col-md-3">
+                    <div class="card card-body">
+                        <img class="img-fluid" src="http://placehold.it/380?text=8">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <button class="carousel-control-prev" type="button" data-bs-target="#artist-carousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#artist-carousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
+    <script>
+        $('.carousel .carousel-item').each(function () {
+            var minPerSlide = 44;
+            var next = $(this).next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
+            next.children(':first-child').clone().appendTo($(this));
+
+            for (var i = 0; i < minPerSlide; i++) {
+                next = next.next();
+                if (!next.length) {
+                    next = $(this).siblings(':first');
+                }
+                next.children(':first-child').clone().appendTo($(this));
+            }
+        });
+    </script>
+<?php
 if (isset($_SESSION['user'])) {
     echo "utente loggato";
 } else {
     echo "utente non loggato";
 }
+
+
 require_once "footer.html";
