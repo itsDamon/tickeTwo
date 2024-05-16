255ecb14c473b609840d84b40defee1daed01a00
MirkoDavenia <mirkettorussel@gmail.com>
2024-05-16 12:03:46 +0200

    Aggiunta eventi (formula 1, show di Andrea Pucci,)

--- sql/database.sql
+++ sql/database.sql
@@ -34,14 +34,14 @@ CREATE TABLE `locations`
 
 CREATE TABLE `events`
 (
-    `id`            INT          NOT NULL AUTO_INCREMENT,
-    `name`          VARCHAR(30)  NOT NULL,
-    `description`   VARCHAR(255) NOT NULL,
-    `date`          TIMESTAMP    NOT NULL,
-    `over_eighteen` BOOL         NOT NULL,
-    `location_id`   INT          NOT NULL,
-    `performer_id`  INT          NOT NULL,
-    type ENUM('CONCERT','THEATRE','SPORT') NOT NULL,
+    `id`            INT                                NOT NULL AUTO_INCREMENT,
+    `name`          VARCHAR(30)                        NOT NULL,
+    `description`   VARCHAR(255)                       NOT NULL,
+    `date`          TIMESTAMP                          NOT NULL,
+    `over_eighteen` BOOL                               NOT NULL,
+    `location_id`   INT                                NOT NULL,
+    `performer_id`  INT                                NOT NULL,
+    type            ENUM ('CONCERT','THEATRE','SPORT') NOT NULL,
     FOREIGN KEY (`performer_id`) REFERENCES performers (`id`),
     FOREIGN KEY (`location_id`) REFERENCES locations (`id`),
     PRIMARY KEY (`id`)
@@ -69,24 +69,38 @@ INSERT INTO `performers` (`stage_name`, `biography`)
 VALUES ('Metallica', 'Metal band'),
        ('Nirvana', 'The best band ever existed'),
        ('System of a Down', 'A Nu Metal Band'),
-       ('Korn',' 90s Nu metal band '),
-        ('Slipknot','They hate people '),
-        ('Caparezza','Famous italian singer'),
-        ('Andrea Pucci','A famous italian cabaretist from milan'),
-        ('Rammstein','The greatest german metal band'),
-        ('Formula 1','Follow the formula 1 everywhere it goes');
+       ('Korn', ' 90s Nu metal band '),
+       ('Slipknot', 'They hate people '),
+       ('Caparezza', 'Famous italian singer'),
+       ('Andrea Pucci', 'A famous italian cabaretist from milan'),
+       ('Rammstein', 'The greatest german metal band'),
+       ('Formula 1', 'Follow the formula 1 everywhere it goes');
 
 INSERT INTO `events` (name, description, date, over_eighteen, location_id, performer_id)
-VALUES ('Sick New World', 'Metallica concert', '2024-05-29', false, (SELECT id
-                                                                     FROM locations
-                                                                     WHERE locations.name LIKE 'Ippodromo SNAI'),
-        (SELECT id
-         FROM performers
-         WHERE stage_name LIKE 'Metallica')),
-       ('Mario', 'Mario concert', '2024-8-30', false, (SELECT id
-                                                       FROM locations
-                                                       WHERE locations.name LIKE 'Unipol Forum'),
-        (SELECT id
-         FROM performers
-         WHERE stage_name LIKE 'Nirvana'));
+VALUES
+    #Concerto dei Metallica
+    ('Sick New World', 'Metallica concert', '2024-05-29', false, (SELECT id
+                                                                  FROM locations
+                                                                  WHERE locations.name LIKE 'Ippodromo SNAI'),
+     (SELECT id
+      FROM performers
+      WHERE stage_name LIKE 'Metallica')),
+
+    #Concerto di Mario
+    ('Mario', 'Mario concert', '2024-8-30', false, (SELECT id
+                                                    FROM locations
+                                                    WHERE locations.name LIKE 'Unipol Forum'),
+     (SELECT id
+      FROM performers
+      WHERE stage_name LIKE 'Nirvana')),
+
+    #Formula 1 insert
+    ('Formula 1', 'Formula 1 at Monza National Racetrack', '2024-06-30', false,
+     (select locations.id from locations where locations.name like 'Autodromo Nazionale di Monza'),
+     (select id from performers where performers.stage_name like 'Formula 1')),
+
+    #Pucci show
+    ('Il meglio del peggio', 'Cabaret show of Andrea Pucci', '2024-09-21', true,
+     (select locations.id from locations where locations.name like 'Teatro Manzoni'),
+     (select id from performers where performers.stage_name like 'Andrea Pucci'));
 
